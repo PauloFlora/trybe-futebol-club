66b82d90fe4a5d61e43d0a4211354a9ae1024582
paulo <tflora.2004@gmail.com>
2022-05-04 15:13:13 -0300

    feat: adds tests for login failure and login GET Route.

--- app/backend/src/tests/login.test.ts
+++ app/backend/src/tests/login.test.ts
@@ -23,3 +23,33 @@ describe('Quando o Login é feito com sucesso', () => {
     expect(username).to.be.equal('Admin');
   })
 });
+
+describe('Quando o Login falhar', () => {
+  it('por não ter sido enviado um email, recebe o status 400', async () => {
+    const chaiHttpResponse: Response = await chai.request(app).post('/login')
+    .send({ password: 'secret_admin' })
+
+    expect(chaiHttpResponse.status).to.be.equal(400);
+  })
+});
+
+describe('Ao fazer uma requisição GET ao Login com um token válido', () => {
+  it('retorna o papel do usuário', async () => {
+    const chaiHttpPOSTResponse: Response = await chai.request(app).post('/login')
+    .send({ email: 'admin@admin.com', password: 'secret_admin' })
+
+    const { user: { username }, token } = chaiHttpPOSTResponse.body;
+
+    const chaiHttpGETResponse: Response = await chai.request(app).get('/login/validate').set('authorization', token)
+
+    //set utilizado aqui para definir a chave authorization e passar o token
+
+    const data = chaiHttpGETResponse.body;
+
+    console.log(data);
+    
+
+    expect(chaiHttpGETResponse.status).to.be.equal(200);
+    expect(data).to.be.equal('admin');
+  })
+});
